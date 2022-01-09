f635a50c98a4c6065a2980e456f0f7bcf3c4a994
Ronuk Raval <ronuk.raval@gmail.com>
2022-01-09 18:26:48 -0500

    Nix flake environment

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "git-nomad";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: let
+    eachSystem = fn: flake-utils.lib.eachDefaultSystem (system:
+      fn nixpkgs.legacyPackages.${system}
+    );
+  in eachSystem (pkgs: {
+    devShell = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        nodePackages.npm
+      ];
+    };
+  });
+}
